cf77826a31f11909fd837cb18ffc6408111ec117
Usercyborg <usercyborg@arena97.local>
2022-12-13 23:10:55 +0530

    Added force destory in s3 Added new file to EC2

--- src/S3/main.tf
+++ src/S3/main.tf
@@ -8,8 +8,12 @@ variable "s3_object_name" {
     default = "singam.jpg"
 }
 
+variable "force_destroy" {
+  default = true
+}
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.s3_bucket_name
+  force_destroy = var.force_destroy
 
   tags = {
     Name        = var.s3_bucket_name
@@ -20,4 +24,6 @@ resource "aws_s3_bucket_object" "sample_file" {
     bucket = var.s3_bucket_name
     key = var.s3_object_name
     # path = "."
+    force_destroy = var.force_destroy
+
 }
